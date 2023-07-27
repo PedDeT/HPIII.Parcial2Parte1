9e32375fcab543b1699bc824af2f0bac93398598
PedDeT <47749943+PedDeT@users.noreply.github.com>
2023-07-27 17:04:38 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button7 = New Button()
+        Button4 = New Button()
+        Button1 = New Button()
+        Button8 = New Button()
+        Button5 = New Button()
+        Button2 = New Button()
+        Button9 = New Button()
+        Button6 = New Button()
+        Button3 = New Button()
+        ButtonC = New Button()
+        ButtonCE = New Button()
+        ButtonDividir = New Button()
+        ButtonRestar = New Button()
+        ButtonMultiplicar = New Button()
+        ButtonSumar = New Button()
+        Button0 = New Button()
+        ButtonDecimal = New Button()
+        ButtonIgual = New Button()
+        ButtonPorcentaje = New Button()
+        txtDisplay = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(55, 142)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 29)
+        Button7.TabIndex = 0
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(55, 196)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 1
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(55, 264)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(214, 142)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(94, 29)
+        Button8.TabIndex = 3
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(214, 196)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 4
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(214, 264)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 5
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(369, 142)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 29)
+        Button9.TabIndex = 6
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(369, 196)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 29)
+        Button6.TabIndex = 7
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(369, 264)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 8
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonC
+        ' 
+        ButtonC.Location = New Point(516, 142)
+        ButtonC.Name = "ButtonC"
+        ButtonC.Size = New Size(94, 29)
+        ButtonC.TabIndex = 9
+        ButtonC.Text = "C"
+        ButtonC.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonCE
+        ' 
+        ButtonCE.Location = New Point(636, 142)
+        ButtonCE.Name = "ButtonCE"
+        ButtonCE.Size = New Size(94, 29)
+        ButtonCE.TabIndex = 10
+        ButtonCE.Text = "CE"
+        ButtonCE.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDividir
+        ' 
+        ButtonDividir.Location = New Point(516, 196)
+        ButtonDividir.Name = "ButtonDividir"
+        ButtonDividir.Size = New Size(94, 29)
+        ButtonDividir.TabIndex = 11
+        ButtonDividir.Text = "/"
+        ButtonDividir.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonRestar
+        ' 
+        ButtonRestar.Location = New Point(636, 196)
+        ButtonRestar.Name = "ButtonRestar"
+        ButtonRestar.Size = New Size(94, 29)
+        ButtonRestar.TabIndex = 12
+        ButtonRestar.Text = "-"
+        ButtonRestar.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonMultiplicar
+        ' 
+        ButtonMultiplicar.Location = New Point(516, 264)
+        ButtonMultiplicar.Name = "ButtonMultiplicar"
+        ButtonMultiplicar.Size = New Size(94, 29)
+        ButtonMultiplicar.TabIndex = 13
+        ButtonMultiplicar.Text = "x"
+        ButtonMultiplicar.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonSumar
+        ' 
+        ButtonSumar.Location = New Point(636, 264)
+        ButtonSumar.Name = "ButtonSumar"
+        ButtonSumar.Size = New Size(94, 29)
+        ButtonSumar.TabIndex = 14
+        ButtonSumar.Text = "+"
+        ButtonSumar.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Location = New Point(55, 344)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(253, 29)
+        Button0.TabIndex = 15
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDecimal
+        ' 
+        ButtonDecimal.Location = New Point(369, 344)
+        ButtonDecimal.Name = "ButtonDecimal"
+        ButtonDecimal.Size = New Size(94, 29)
+        ButtonDecimal.TabIndex = 16
+        ButtonDecimal.Text = "."
+        ButtonDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonIgual
+        ' 
+        ButtonIgual.Location = New Point(516, 344)
+        ButtonIgual.Name = "ButtonIgual"
+        ButtonIgual.Size = New Size(94, 29)
+        ButtonIgual.TabIndex = 17
+        ButtonIgual.Text = "="
+        ButtonIgual.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonPorcentaje
+        ' 
+        ButtonPorcentaje.Location = New Point(636, 344)
+        ButtonPorcentaje.Name = "ButtonPorcentaje"
+        ButtonPorcentaje.Size = New Size(94, 29)
+        ButtonPorcentaje.TabIndex = 18
+        ButtonPorcentaje.Text = "%"
+        ButtonPorcentaje.UseVisualStyleBackColor = True
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.BackColor = SystemColors.Info
+        txtDisplay.Font = New Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point)
+        txtDisplay.Location = New Point(55, 49)
+        txtDisplay.MaximumSize = New Size(675, 75)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.ReadOnly = True
+        txtDisplay.Size = New Size(675, 63)
+        txtDisplay.TabIndex = 19
+        txtDisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtDisplay)
+        Controls.Add(ButtonPorcentaje)
+        Controls.Add(ButtonIgual)
+        Controls.Add(ButtonDecimal)
+        Controls.Add(Button0)
+        Controls.Add(ButtonSumar)
+        Controls.Add(ButtonMultiplicar)
+        Controls.Add(ButtonRestar)
+        Controls.Add(ButtonDividir)
+        Controls.Add(ButtonCE)
+        Controls.Add(ButtonC)
+        Controls.Add(Button3)
+        Controls.Add(Button6)
+        Controls.Add(Button9)
+        Controls.Add(Button2)
+        Controls.Add(Button5)
+        Controls.Add(Button8)
+        Controls.Add(Button1)
+        Controls.Add(Button4)
+        Controls.Add(Button7)
+        Name = "Form1"
+        RightToLeft = RightToLeft.No
+        Text = "Calculadora"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ButtonC As Button
+    Friend WithEvents ButtonCE As Button
+    Friend WithEvents ButtonDividir As Button
+    Friend WithEvents ButtonRestar As Button
+    Friend WithEvents ButtonMultiplicar As Button
+    Friend WithEvents ButtonSumar As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonDecimal As Button
+    Friend WithEvents ButtonIgual As Button
+    Friend WithEvents ButtonPorcentaje As Button
+    Friend WithEvents txtDisplay As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Drawing.Text
+
+Public Class Form1
+    Dim numero1 As Double
+    Dim numero2 As Double
+    Dim resultado As Double
+    Dim [operador] As String
+
+    Private Sub ButtonNum_Click(sender As Object, e As EventArgs) Handles Button0.Click, Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+        Dim button As Button = sender
+        txtDisplay.Text &= button.Text
+    End Sub
+
+    Private Sub buttonDecimal_Click(sender As Object, e As EventArgs) Handles ButtonDecimal.Click
+        If Not txtDisplay.Text.Contains(".") Then
+            txtDisplay.Text &= "."
+        End If
+    End Sub
+
+    Private Sub buttonSumar_Click(sender As Object, e As EventArgs) Handles ButtonSumar.Click, ButtonRestar.Click, ButtonMultiplicar.Click, ButtonDividir.Click, ButtonPorcentaje.Click
+        Dim button As Button = sender
+        If button.Text = "%" Then
+            numero1 = Val(txtDisplay.Text) / 100
+            txtDisplay.Text = numero1.ToString()
+        Else
+            numero1 = Val(txtDisplay.Text)
+            [operador] = button.Text
+            txtDisplay.Text = ""
+        End If
+    End Sub
+
+    Private Sub buttonIgual_Click(sender As Object, e As EventArgs) Handles ButtonIgual.Click
+        numero2 = Val(txtDisplay.Text)
+        Select Case [operador]
+            Case "+"
+                resultado = numero1 + numero2
+            Case "-"
+                resultado = numero1 - numero2
+            Case "x"
+                resultado = numero1 * numero2
+            Case "/"
+                resultado = numero1 / numero2
+            Case "%"
+                resultado = numero1 / 100
+        End Select
+        txtDisplay.Text = resultado.ToString
+    End Sub
+
+    Private Sub buttonC_Click(sender As Object, e As EventArgs) Handles ButtonC.Click
+        txtDisplay.Text = ""
+        numero2 = 0
+    End Sub
+
+    Private Sub buttonCE_Click(sender As Object, e As EventArgs) Handles ButtonCE.Click
+        txtDisplay.Text = ""
+        numero1 = 0
+        numero2 = 0
+        [operador] = ""
+    End Sub
+End Class
